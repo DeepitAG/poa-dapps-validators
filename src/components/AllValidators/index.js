56c41ddc56eae795045ec19e42f26d69928d5f10
Arsenii Petrovich <arseniy.altoros@gmail.com>
2019-10-23 00:14:00 +0300

    Update index.js

--- src/components/AllValidators/index.js
+++ src/components/AllValidators/index.js
@@ -44,7 +44,6 @@ export default class AllValidators extends Component {
       })
   }
   async augmentValidatorsWithPhysicalAddress(validators) {
-    return validators
     let augmentedValidators = []
 
     const popa = this.getProofOfPhysicalAddressContract()
