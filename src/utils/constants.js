c776f5a44d046e17cc6fa26c196fae2ea0fd7f56
Arsenii Petrovich <arseniy.altoros@gmail.com>
2019-10-23 02:06:54 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,6 +11,10 @@ constants.ABIsSources = {
 constants.userDeniedTransactionPattern = 'User denied transaction'
 constants.rootPath = '/poa-dapps-validators'
 constants.branches = {
+  DAI: 'dai',
+  CORE: 'core',
+  SOKOL: 'sokol',
+  KOVAN: 'kovan',
   PCHAIN: 'pchain'
 }
 
@@ -33,6 +37,30 @@ constants.navigationData = [
 ]
 
 constants.NETWORKS = {
+  '42': {
+    NAME: 'Kovan',
+    RPC: 'https://kovan.infura.io/v3/1125fe73d87c4e5396678f4e3089b3dd',
+    BRANCH: constants.branches.KOVAN,
+    TESTNET: true
+  },
+  '77': {
+    NAME: 'Sokol',
+    RPC: 'https://sokol.poa.network',
+    BRANCH: constants.branches.SOKOL,
+    TESTNET: true
+  },
+  '99': {
+    NAME: 'Core',
+    RPC: 'https://core.poa.network',
+    BRANCH: constants.branches.CORE,
+    TESTNET: false
+  },
+  '100': {
+    NAME: 'Dai',
+    RPC: 'https://dai.poa.network',
+    BRANCH: constants.branches.DAI,
+    TESTNET: false
+  },
   '191': {
     NAME: 'Pchain',
     RPC: 'http://bootnode-balancer-820434300.us-east-1.elb.amazonaws.com',
